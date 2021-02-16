7aaeefc8581150e40a1c064cc4f6e5c66286a9ee
Rachael Blake <rblake@rstudio03.research.sesync.org>
2021-02-16 15:59:50 -0500

    get values outside function

--- boliv_neighbors.R
+++ boliv_neighbors.R
@@ -7,15 +7,18 @@ library(raster)
 ### Open data
 boliv <- raster("/nfs/agfrontiers-data/Remote Sensing/KS files/classi_bol_dry_2008_102033.tif")
 
+# get values for every cell in the raster
+b_values <- getValues(boliv)
+
 ### Function to calculate % neighbors of different types
-percent_different_neighbors <- function(cell) {
+percent_different_neighbors <- function(cell, values) {
   # pull neighboring values
   # see ?adjacent for the arguments used here
-  x <- getValues(boliv)[adjacent(boliv, 
-                                 cells = cell, 
-                                 directions = 8, 
-                                 pairs = F, 
-                                 include = T)]
+  x <- adjacent(values, 
+                cells = cell, 
+                directions = 8, 
+                pairs = F, 
+                include = T)
   # calculate the percent of them that are not equal to the focal cell (i.e., not equal to the first value of x)
   perc_diff <- sum(x!=x[1])/(length(x)-1)*100
   return(perc_diff)
@@ -23,7 +26,7 @@ percent_different_neighbors <- function(cell) {
 
 ### Run on sample matrix: apply function to each cell
 perc_diff_values <- map_dbl(1:ncell(boliv),
-                            ~percent_different_neighbors(.))
+                            ~percent_different_neighbors(., b_values))
 
 ### Make a new raster with the cells
 perc_diff_raster <- boliv %>% setValues(perc_diff_values)
